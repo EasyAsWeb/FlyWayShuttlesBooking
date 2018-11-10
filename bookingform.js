197b2eb14f2e4e855d12c47a329af552a45167ac
sbhol <sbhol@users.noreply.github.com>
2018-11-11 01:38:34 +0530

    Update bookingform.js

--- bookingform.js
+++ bookingform.js
@@ -79,7 +79,7 @@ var errc=0;
 						
                         $('#ticket').text('From :'+origin+
                         " \nTo :" + destination +"\nDistance :"+ distance_in_kilo +" kms " + "\nFare: "+ fare + "\nDate-Time:"+ dttm_val);
-                       $('#Buyticket').data('item-quantity', distance_in_kilo);
+                       $('#Buyticket').data('item-name', distance_in_kilo);
 					   $('#Buyticket').data('item-description', origin +"::to::" +destination);
 					   
 						document.getElementById('welcomeDiv').style.display = "block";
